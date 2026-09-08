bb664d65af16d3a3b14265f932cbbd9b36079bcf
Fant0men <minifruttiman@gmail.com>
2026-09-08 15:52:12 +0200

    Update malartag_bevakning.py

--- malartag_bevakning.py
+++ malartag_bevakning.py
@@ -417,7 +417,7 @@ def build_html_report(
 <link rel="stylesheet" href="{css_href}">
 </head>
 <body>
-  <h1>Mälartåg-bevakning</h1>
+  <h1>Mälartåg-mañana</h1>
   <p class="meta">Södertälje Syd ↔ Eskilstuna C &middot; Rapport för {report_date}</p>
   {nav_html}
   <table>
